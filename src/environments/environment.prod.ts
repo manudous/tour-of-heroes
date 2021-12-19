65fd441af16dd769c24e5421762df12eafbfff4d
manudous <you@example.com>
2021-12-19 12:43:30 +0100

    change connections

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  containerName: "alteregos",
-  apiUrl: "https://tour-of-heroes-free-account.azurewebsites.net/api/hero",
-  storageUrl: "https://lemonheroes.blob.core.windows.net"
+  containerName: 'alteregos',
+  apiUrl: 'https://tour-of-heroes-api.azurewebsites.net/api/hero',
+  storageUrl: 'https://lemonheroes2.blob.core.windows.net',
 };
